562414930d13b1000ede6a06eb823da33947b0bb
Rain Haworth <rainnhaworth@gmail.com>
2024-05-23 15:07:41 -0400

    improved splitmulti; added test set subsampling and bin exponent arguments

--- deep-conus/splitmulti_future.py
+++ deep-conus/splitmulti_future.py
@@ -6,11 +6,25 @@ import pickle
 import numpy as np
 import xarray as xr
 import argparse
+import random
 
 # Create splits on decomposed data as dictionaries of indices
-def split_data_future(datasrc, futuresrc, filestr,
-               n_class, n_class_future, class_sep, domain_gap, val_sz, max_val):
+def split_data_future(args):
     # NOTE: data where max UH exceeds max_val will still be placed in the last class
+
+    # get variables from args
+    datasrc = args.datasrc
+    futuresrc = args.futuresrc
+    filestr = args.filestr
+    n_class = args.n_class
+    n_class_future = args.n_class_future
+    sample_future = args.sample_future
+    class_sep = args.class_sep
+    domain_gap = args.domain_gap
+    val_sz = args.val_sz
+    max_val = args.max_val
+    exp = args.exp
+
 
     # Calculate (past storm) class size
     class_size = max_val / n_class
@@ -18,7 +32,6 @@ def split_data_future(datasrc, futuresrc, filestr,
           .format(max_val, class_size, class_size*(1-class_sep)))
 
     # Define class function: get floor from class number
-    exp = 3
     def class_val(n, mx, n_c):
         return mx * ((n / n_c) ** exp)
 
@@ -48,16 +61,18 @@ def split_data_future(datasrc, futuresrc, filestr,
 
     print("source_cls:", len(source_cls), source_cls)
     print("val_cls   :", len(val_cls), val_cls)
-    print("target_cls:", len(target_cls), target_cls)
+    print("target_cls:", len(target_cls), list(target_cls))
 
     count_class = [0] * n_class
     timestamp = str(time.time())
 
     # Open label list
-    if os.path.isfile(datasrc + filestr + '.csv') and os.path.isfile(futuresrc + filestr + '.csv'):
+    past_path = os.path.join(datasrc, filestr + '.csv')
+    future_path = os.path.join(futuresrc, filestr + '.csv')
+    if os.path.isfile(past_path) and os.path.isfile(future_path):
         # Load training set
         uh25mx = []
-        with open(datasrc + filestr + '.csv', 'rb') as f:
+        with open(past_path, 'r') as f:
             uh25mx = np.loadtxt(f, delimiter=',')
 
         # Create list of classes; indices of uh_class must match patch indices
@@ -94,10 +109,9 @@ def split_data_future(datasrc, futuresrc, filestr,
             elif uh_class[i] in val_cls:
                 val_dict.update({i: uh_class[i]})
 
-
         # Load test set
         uh25mx_test = []
-        with open(futuresrc + filestr + '.csv', 'rb') as f:
+        with open(future_path, 'r') as f:
             uh25mx_test = np.loadtxt(f, delimiter=',')
 
         # Calculate future max and class size
@@ -106,7 +120,10 @@ def split_data_future(datasrc, futuresrc, filestr,
 
         tgt_dict = {}
         count_class = [0] * n_class_future
-        for i in range(len(uh25mx_test)):
+
+        # subsample future storms so we don't have 1.4m test samples
+        test_samples = len(uh25mx_test)
+        for i in random.sample(range(test_samples), int(test_samples * sample_future)):
             val = uh25mx_test[i]
             # Actual class size = class_size * (1 - separation)
             val_class = math.floor(class_num(val, future_max, n_class_future))
@@ -134,7 +151,7 @@ def split_data_future(datasrc, futuresrc, filestr,
         with open(futuresrc + "tgt_split_" + timestamp + '.pickle','wb') as f:
             pickle.dump(tgt_dict, f)
 
-        print("Dataset split with validation complete. Past storms dictionary stored at", datasrc, "and future storms at ", futuresrc)
+        print("Dataset split with validation complete. Past storms dictionary stored at", datasrc, "and future storms at", futuresrc)
         print("Timestamp:", timestamp)
     else:
         print("Uh25max files not found. Run decompose.py on past storms and future storms, each in different directories.")
@@ -146,15 +163,15 @@ parser.add_argument('--datasrc',        type=str,   default='./data/',          
 parser.add_argument('--futuresrc',      type=str,   default='./data-future/',   help='set future storm dataset source directory')
 parser.add_argument('--filestr',        type=str,   default='uh25max',          help='set directory filename (expects $(filestr).csv)')
 parser.add_argument('--n_class',        type=int,   default=15,                 help='number of classes to create when binning past storms')
-parser.add_argument('--n_class_future', type=int,   default=5,                 help='number of classes to create when binning')
-parser.add_argument('--max_val',        type=int,   default=75,                 help='max value used when binning classes')
+parser.add_argument('--n_class_future', type=int,   default=5,                  help='number of classes to create when binning')
+parser.add_argument('--sample_future',  type=float, default=0.01,               help='fraction of future storms to sample')
+parser.add_argument('--max_val',        type=float, default=75.0,               help='max value used when binning classes')
 parser.add_argument('--class_sep',      type=float, default=0.5,                help='separation between classes')
-parser.add_argument('--val_sz',         type=float, default=0.35,                help='validation split')
+parser.add_argument('--val_sz',         type=float, default=0.35,               help='validation split')
 parser.add_argument('--domain_gap',     type=float, default=1.0,                help='domain gap, i.e. difference between splits; 0.0 is lowest, 1.0 is highest')
+parser.add_argument('--exp',            type=float, default=1.0,                help='binning exponent, e.g. 2.0 = quadratic')
 
 args = parser.parse_args()
 
 # Run split function
-split_data_future(datasrc=args.datasrc, futuresrc=args.futuresrc, filestr=args.filestr,
-                      n_class=args.n_class, n_class_future=args.n_class_future, max_val=args.max_val,
-                      class_sep=args.class_sep, val_sz=args.val_sz, domain_gap=args.domain_gap)
+split_data_future(args)
